fb9fb22c4434f50613746fa86c78c56645842804
espectral55 <alvsosa@ucm.es>
2026-03-03 18:35:51 +0100

    Avance en Logica enemigo
    El enemigo tiene un cono de visión que si detecta al jugador, lo persigue.

--- Assets/Scripts/Components/EnemyLogic.cs
+++ Assets/Scripts/Components/EnemyLogic.cs
@@ -1,5 +1,5 @@
 //---------------------------------------------------------
-// Breve descripción del contenido del archivo
+// Script que controla los estados y la forma de actuar de los enemigos.
 // Alvaro Sosa Rodriguez
 // Bouquet Of Sins
 // Proyectos 1 - Curso 2025-26
@@ -30,11 +30,10 @@ public class EnemyLogic : MonoBehaviour
     [SerializeField]
     private Transform[] Positions;
     /// <summary>
-    /// El atributo speed define la velocidad a la que se mueve el enemigo.
+    /// El atributo Speed define la velocidad a la que se mueve el enemigo.
     /// </summary>
     [SerializeField]
     private float Speed;
-
     #endregion
 
     // ---- ATRIBUTOS PRIVADOS ----
@@ -52,15 +51,25 @@ public class EnemyLogic : MonoBehaviour
     #endregion
     private int _posIndex = 0;
     /// <summary>
-    /// La variable _heardNoise determina si el enemigo a entrado en contacto con un sonido.
+    /// La variable _heardNoise determina si el enemigo ha entrado en contacto con un sonido.
     /// </summary>
     private bool _heardNoise = false;
     /// <summary>
     /// La variable _isPlayerInRange determina si el jugador está dentro del rango de vision.
     /// </summary>
     private bool _isPlayerInRange = false;
-
+    /// <summary>
+    /// 
+    /// </summary>
+    private bool _isPlayerVisible = false;
+    /// <summary>
+    /// La variable _noiseOrigin determina la posicion donde se origina el sonido con el que choca el enemigo.
+    /// </summary>
     private Transform _noiseOrigin;
+    /// <summary>
+    /// Determina la posición del jugador cuando entra en el campo de vision del enemigo.
+    /// </summary>
+    private Transform _playerPos;
     
     // ---- MÉTODOS DE MONOBEHAVIOUR ----
     #region Métodos de MonoBehaviour
@@ -94,6 +103,12 @@ public class EnemyLogic : MonoBehaviour
     // mayúscula, incluida la primera letra)
     // Ejemplo: GetPlayerController
 
+    public void SawThePlayer(Transform playerPos)
+    {
+        _playerPos = playerPos;
+        _isPlayerVisible = true;
+    }
+
     #endregion
 
     // ---- MÉTODOS PRIVADOS ----
@@ -102,18 +117,9 @@ public class EnemyLogic : MonoBehaviour
     // El convenio de nombres de Unity recomienda que estos métodos
     // se nombren en formato PascalCase (palabras con primera letra
     // mayúscula, incluida la primera letra)
-    /// <summary>
-    /// Se encarga de mover al enemigo de una posicion a otra a cierta velocidad.
-    /// </summary>
-    /// <param name="speed"></param>
-    /// <param name="time"></param>
-    /// <param name="i"></param>
-    /// 
-
-
 
     /// <summary>
-    /// 
+    /// El metodo PerformPatrol se encarga de mover al enemigo en las posiciones indicadas en el array Positions.
     /// </summary>
     private void PerformPatrol()
     {
@@ -127,19 +133,21 @@ public class EnemyLogic : MonoBehaviour
         } 
     }
     /// <summary>
-    /// 
+    /// El metodo UpdateEnemyState maneja los estados del enemigo.
     /// </summary>
     private void UpdateEnemyState()
     {
-        if (!_heardNoise && !_isPlayerInRange)
+        if (!_heardNoise && !_isPlayerVisible && !_isPlayerInRange)
         {
             PerformPatrol();
         }
-        else if (_heardNoise && !_isPlayerInRange)
+        else if (_heardNoise && !_isPlayerVisible && !_isPlayerInRange || _isPlayerVisible && !_isPlayerInRange)
         {
-            StartCoroutine(PerformChase());
+            if(_isPlayerVisible) StartCoroutine(PerformChase(_playerPos));
+            else if (_heardNoise) StartCoroutine(PerformChase(_noiseOrigin));
+
         }
-        else if (_heardNoise && _isPlayerInRange)
+        else if (_isPlayerInRange)
         {
             PerformAttack();
         }
@@ -148,11 +156,10 @@ public class EnemyLogic : MonoBehaviour
     /// <summary>
     /// Si el enemigo detecta un sonido, este se movera al origen del mismo.
     /// </summary>
-    private IEnumerator PerformChase()
+    private IEnumerator PerformChase(Transform positionToGo)
     {
         yield return new WaitForSeconds(1f);
-        Debug.Log("Chase");
-        transform.position = Vector3.MoveTowards(transform.position, _noiseOrigin.position, Speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, positionToGo.position, Speed * Time.deltaTime);
     }
 
     /// <summary>
@@ -164,7 +171,7 @@ public class EnemyLogic : MonoBehaviour
     }
 
     /// <summary>
-    /// Detecta si el enemigo entra en contacto con una onda de sonido.
+    /// Detecta si el enemigo entra en contacto con una onda de sonido o con el jugador.
     /// </summary>
     /// <param name="collision"></param>
     private void OnTriggerEnter2D(Collider2D collision)
@@ -177,6 +184,9 @@ public class EnemyLogic : MonoBehaviour
             _noiseOrigin = noiseCircle.gameObject.transform;
             _heardNoise = true;
         }
+
+        // Hacer Ducktyping para saber si es el jugador. Si lo es, poner _isPlayerInRange a true.
+
     }
     #endregion   
 
---
+++ Assets/Scripts/Components/EnemyVision.cs
@@ -0,0 +1,102 @@
+//---------------------------------------------------------
+// Script que comunica al script "EnemyLogic" si el jugador se encuentra en el campo de visión del enemigo.
+// Álvaro Sosa Rodríguez
+// Bouquet Of Sins
+// Proyectos 1 - Curso 2025-26
+//---------------------------------------------------------
+
+using UnityEngine;
+// Añadir aquí el resto de directivas using
+
+
+/// <summary>
+/// Antes de cada class, descripción de qué es y para qué sirve,
+/// usando todas las líneas que sean necesarias.
+/// </summary>
+public class EnemyVision : MonoBehaviour
+{
+    // ---- ATRIBUTOS DEL INSPECTOR ----
+    #region Atributos del Inspector (serialized fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // públicos y de inspector se nombren en formato PascalCase
+    // (palabras con primera letra mayúscula, incluida la primera letra)
+    // Ejemplo: MaxHealthPoints
+
+    #endregion
+
+    // ---- ATRIBUTOS PRIVADOS ----
+    #region Atributos Privados (private fields)
+    // Documentar cada atributo que aparece aquí.
+    // El convenio de nombres de Unity recomienda que los atributos
+    // privados se nombren en formato _camelCase (comienza con _, 
+    // primera palabra en minúsculas y el resto con la 
+    // primera letra en mayúsculas)
+    // Ejemplo: _maxHealthPoints
+
+    #endregion
+
+    // ---- MÉTODOS DE MONOBEHAVIOUR ----
+    #region Métodos de MonoBehaviour
+
+    // Por defecto están los típicos (Update y Start) pero:
+    // - Hay que añadir todos los que sean necesarios
+    // - Hay que borrar los que no se usen 
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before 
+    /// any of the Update methods are called the first time.
+    /// </summary>
+    void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+
+    }
+    #endregion
+
+    // ---- MÉTODOS PÚBLICOS ----
+    #region Métodos públicos
+    // Documentar cada método que aparece aquí con ///<summary>
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+    // Ejemplo: GetPlayerController
+
+
+
+    #endregion
+
+    // ---- MÉTODOS PRIVADOS ----
+    #region Métodos Privados
+    // Documentar cada método que aparece aquí
+    // El convenio de nombres de Unity recomienda que estos métodos
+    // se nombren en formato PascalCase (palabras con primera letra
+    // mayúscula, incluida la primera letra)
+
+    /// <summary>
+    /// Este metodo detecta si el jugador se encuentra en el rango de vision del enemigo y manda la posicion del jugador.
+    /// </summary>
+    /// <param name="collision"></param>
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        playerMovement playerScript = collision.gameObject.GetComponent<playerMovement>();
+        if (playerScript != null)
+        {
+            // Avisar a EnemyLogic que el player esta en el rango de vision
+            Debug.Log("Player detected");
+            EnemyLogic enemyLogic = transform.parent.GetComponent<EnemyLogic>();
+            enemyLogic.SawThePlayer(playerScript.gameObject.transform);
+        }
+    }
+
+    #endregion
+
+} // class EnemyVision 
+// namespace
